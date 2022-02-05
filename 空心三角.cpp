5335c165de6161ed3c49313b69c5037f6a33766d
621352466 <96056539+621352466@users.noreply.github.com>
2022-02-06 00:16:54 +0800

    1

---
+++ 空心三角.cpp
@@ -0,0 +1,31 @@
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//
+//    int i = 0;
+//    cin >> i;
+//    cout << "*" << endl;
+//    int k = i;
+//    for (int j = 1; j <= i - 2; j++)
+//    {
+//        cout << "* ";
+//        if (j == k)
+//        {
+//            goto flag;
+//        }
+//        for (k = 1; k < j; k++)
+//        {
+//            cout << "  ";
+//        }
+//        cout << "*" << endl;
+//    }
+//flag:
+//    for (int l = 0; l < i; l++)
+//    {
+//        cout << "* ";
+//    }
+//    system("pause");
+//
+//    return 0;
+//}
